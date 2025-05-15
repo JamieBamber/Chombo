f6f055cda7162fc83b34b65cf93be615a82e70d7
Jamie Bamber <jbamber@login3.frontera.tacc.utexas.edu>
2025-05-15 17:30:31 -0500

    make files for MareNostrum

---
+++ lib/mk/modules_load_marenostrum.sh
@@ -0,0 +1,8 @@
+module purge
+module load intel/2023.2.0 impi/2021.10.0 cmake/3.25.1
+module load gsl/2.7.1 hdf5/1.14.1-2
+module load oneapi/2023.2.0
+module load petsc/3.20.5
+#/3.18.0-intel24.1-impi21.12
+#petsc/3.21.0-intel24.0-impi21.11
+module list
